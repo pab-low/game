332a0607ce99c5d0fc26052407012a453ef25b14
pablo <pabloocampopita@gmail.com>
2019-06-20 06:11:22 -0400

    game

--- game.js
+++ game.js
@@ -8,18 +8,22 @@ var cursors;
 var jumpButton;
 var text;
 var winningMessage;
+var LosingMessage;
 var won = false;
-var currentScore = 10;
+var lose = false;
+var currentScore = 0;
 var winningScore = 100 ;
 
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin')
+  createItem(375, 300, 'poison')
   createItem(300, 500, 'coin')
   createItem(500, 500, 'coin')
   createItem(400, 400, 'coin')
   createItem(100, 100, 'coin')
+  createItem(100, 100, 'coin')
+  createItem(300, 100, 'coin')
   createItem(300, 100, 'coin')
   createItem(100, 500, 'coin')
   createItem(500, 100, 'coin')
@@ -37,12 +41,12 @@ function addPlatforms() {
   platforms.create(100, 500, 'platform2');
   platforms.create(100, 450, 'platform2');
   platforms.create(300, 450, 'platform2');
-  platforms.create(500, 150, 'platform');
   platforms.create(250, 154, 'platform2');
   platforms.create(50, 300, 'platform2');
   platforms.create(150, 250, 'platform2');
   platforms.create(625, 300, 'platform2');
   platforms.setAll('body.immovable', true);
+  platforms.create(500, 150, 'platform');
 }
 
 // create a single animated item and add to screen
@@ -81,6 +85,11 @@ if (item.key == 'star'){
   if (currentScore === winningScore) {
       createBadge();
   }
+  
+  if(currentScore < 0){
+    lose = true;
+    player.kill();
+  }
 }
 
 // when the player collects the badge at the end of the game
@@ -88,6 +97,7 @@ function badgeHandler(player, badge) {
   badge.kill();
   won = true;
 }
+
 
 // setup game when the web page loads
 window.onload = function () {
@@ -104,7 +114,7 @@ window.onload = function () {
     
     
     //Load spritesheets
-    game.load.spritesheet('player', 'assets/chalkers.png', 48, 62);
+    game.load.spritesheet('player', 'assets/mikethefrog.png', 31, 31);
     game.load.spritesheet('coin', 'assets/coin.png', 36, 44);
     game.load.spritesheet('badge', 'assets/badge.png', 42, 54);
     game.load.spritesheet('poison', 'assets/poison.png', 32, 32);
@@ -128,6 +138,8 @@ window.onload = function () {
     text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Arial", fill: "white" });
     winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white" });
     winningMessage.anchor.setTo(0.5, 1);
+    LosingMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white" });
+    LosingMessage.anchor.setTo(0.5, 1);
   }
 
   // while the game is running
@@ -158,10 +170,16 @@ window.onload = function () {
     if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
       player.body.velocity.y = -400;
     }
-    // when the player winw the game
+    // when the player wins or loses the game
     if (won) {
-      winningMessage.text = "YOU WIN!!!";
+      winningMessage.text = "YOU WON!!!";
+      player.kill();
     }
+    if (lose){
+      LosingMessage.text = "YOU LOSE!!!";
+    }
+    
+  
   }
 
   function render() {
